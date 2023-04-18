b8c78bb407e355a317a76dcb3a893059a1ae83f4
pengguolong <pengguolong@fanletech.com>
2023-04-18 17:06:10 +0800

    跳过本地检查。

--- app/src/main/java/com/example/pengguolong/testpremithook/MainActivity.kt
+++ app/src/main/java/com/example/pengguolong/testpremithook/MainActivity.kt
@@ -7,8 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+import android.view.*
 import com.example.pengguolong.testpremithook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
